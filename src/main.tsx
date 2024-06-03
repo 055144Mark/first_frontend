324633a11c592c96e0bc7712c39c73c29e8928d9
Mark <msejkis@gmail.com>
2024-06-03 23:45:20 +0300

    Workflow

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from './App.tsx';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-const manifestUrl = "https://raw.githubusercontent.com/055144Mark/exhausted/main/manifest.json";
+const manifestUrl = "https://055144Mark.github.io/first_frontend/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
